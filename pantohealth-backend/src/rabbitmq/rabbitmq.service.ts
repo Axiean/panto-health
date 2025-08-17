078f1df80467eaf607ba85cecc81a8bfdc5051a8
Axiean <axieans@gmail.com>
2025-08-17 15:47:57 +0330

    add logger

--- pantohealth-backend/src/rabbitmq/rabbitmq.service.ts
+++ pantohealth-backend/src/rabbitmq/rabbitmq.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { Injectable, Logger } from '@nestjs/common';
+import { AmqpConnection, RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { ConsumeMessage } from 'amqplib';
 import { SignalsService } from 'src/signals/signals.service';
 import { plainToInstance } from 'class-transformer';
@@ -8,7 +8,12 @@ import { XRayPayloadDto } from 'src/signals/dto';
 
 @Injectable()
 export class RabbitmqService {
-  constructor(private readonly signalsService: SignalsService) {}
+  private readonly logger = new Logger(RabbitmqService.name);
+
+  constructor(
+    private readonly signalsService: SignalsService,
+    private readonly amqpConnection: AmqpConnection,
+  ) {}
 
   @RabbitSubscribe({
     exchange: 'pantohealth-exchange',
@@ -19,8 +24,23 @@ export class RabbitmqService {
     const payload = plainToInstance(XRayPayloadDto, msg);
     const errors = await validate(payload);
 
+    if (errors.length > 0) {
+      this.logger.error({
+        message: 'Invalid message structure received. Rejecting message.',
+        errors: errors,
+        originalMessage: msg,
+      });
+      this.amqpConnection.channel.nack(amqpMsg, false, false);
+      return;
+    }
+
     const deviceId = Object.keys(msg)[0];
     const { data, time } = msg[deviceId];
+
+    this.logger.log({
+      message: `Successfully validated and processing message for device`,
+      deviceId: deviceId,
+    });
 
     await this.signalsService.create(deviceId, time, data, amqpMsg.content);
   }
